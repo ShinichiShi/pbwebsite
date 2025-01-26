62fb3f50dbc4433fa5b6f8b61586addf01ae1760
Nexusrex18 <lavisnj350@gmail.com>
2025-01-26 23:33:14 +0530

    credit button issue fix

--- components/ContributorsBtn.tsx
+++ components/ContributorsBtn.tsx
@@ -6,22 +6,25 @@ export const ContributorsBtn = () => {
   const router = useRouter();
 
   const handleClick = async () => {
-    try {
-      const response = await fetch('/api/credits/newCollaborator', {
-        method: 'POST',
+  
+    fetch('/api/credits/newCollaborator', {
+      method: 'POST',
+    })
+      .then(async (response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch data from GitHub API');
+        }
+      })
+      .catch((err: any) => {
+        console.error('Error posting contributor:', err.message);
       });
-      if (!response.ok) {
-        throw new Error('Failed to fetch data from GitHub API');
-      }
-      router.push('/Credits');
-    } catch (err:any) {
-      console.error('Error posting contributor:', err.message);
-    }
+  
+    router.push('/Credits');
   };
 
   return (
     <div>
-      <button type='button' onClick={handleClick} className="btn-sm px-4 py-2 text-l font-bold bg-gradient-to-tr from-gray-900 to-indigo-800 mx-3 rounded-xl inline-block">
+      <button type='button' onClick={handleClick} className="btn-sm px-4 py-2 text-l font-bold bg-gradient-to-tr from-gray-900 to-indigo-800 hover:from-indigo-800 hover:to-gray-900 active:from-gray-900 active:to-indigo-900 mx-3 rounded-xl inline-block">
         View Credits
       </button>
     </div>
