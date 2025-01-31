a4e181fc5a3164513cca6e3cd632ce2ebef35d6c
Vanisha <jrvanisha123v@gmail.com>
2025-01-31 23:32:34 +0530

    Added different cns to new lines

--- components/carousel.component.js
+++ components/carousel.component.js
@@ -44,7 +44,8 @@ export default function Carousel({ slides, useScrollHoverEffects = false, classN
             src={s} 
             alt="" 
             {...(useScrollHoverEffects ? { width: 500, height: 500 }: {})}
-            className={cn("items-center", useScrollHoverEffects && "w-full h-full object-cover")}  
+            className={cn("items-center", 
+              useScrollHoverEffects && "w-full h-full object-cover")}  
             key={idx} />;
         })}
       </div>
