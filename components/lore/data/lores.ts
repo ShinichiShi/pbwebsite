fb2c89133344fb3e2c0edcbbf629794fe9d801bd
Sky Singh <akashsingh2210670@gmail.com>
2025-05-24 17:04:29 +0530

    Refine lore entries by correcting grammatical errors, enhancing clarity, and updating character references in stories for Coorg and NITK experiences.

--- components/lore/data/lores.ts
+++ components/lore/data/lores.ts
@@ -7,10 +7,10 @@ export const Lores: Lore[] = [
     date: 'April 2025',
     location: 'Coorg, Karnataka',
     preview: 'What did Point Blank do after submitting their GSoC proposals?',
-    story: [ "I mean logically speaking, after grinding for 6 months straight, the only logical escape was in the middle of the mountains. What PB did was simple, Click on the submit button, book a bus and run straight to the middle of nowhere."
-            ,"The Coorg lore consists of the campfire at 1am, The 10km long trek to the middle of the clouds, the nights we spent dancing in the bus, the thousands of inside jokes and the relief that now the results are no longer in our hands.",
-            "That year, PB produced 8 Mentees and 3 Mentors in GSOC, the highest in PB History. As one of the leads of the time Sky Said 'May the lore finally find its peace here', hinting to the fact that when he submitted his propopsal in 2024, the first thing he also did was run straight to Coorg."
-            ,"Point Blank is pretty sure the people at the coffee plantation and the leeches on the Tadiandamol Trek will remember us, that Coorg will remain a piece of PB lore for years to come."
+    story: [ "Logically speaking, after grinding for 6 months straight, the only logical escape was in the middle of the mountains. What PB did was simple, Click on the submit button, book a bus and run straight to the middle of nowhere."
+            ,"The Coorg lore consists of the campfire at 1am, The 10km long trek to the middle of the clouds, the nights they spent dancing in the bus, the thousands of inside jokes and the relief that now the results were no longer in their hands.",
+            "That year, PB produced 8 Mentees and 3 Mentors in GSOC, the highest in PB History. As one of the leads of the time Sky Said 'May the lore finally find its peace here', hinting to the fact that when he submitted his proposal in 2024, the first thing he also did was run straight to Coorg."
+            ,"Point Blank is pretty sure the people at the coffee plantation and the leeches on the Tadiandamol Trek will remember them, that Coorg will remain a piece of PB lore for years to come."
     ],
     images: [
       'https://res.cloudinary.com/pbsite/image/upload/v1745004377/IMG_0123_awx0jq.webp',
@@ -26,15 +26,15 @@ export const Lores: Lore[] = [
     ]
   },
   {
-    id: 'ntik',
-    title: 'Victory at NTIK',
+    id: 'nitk',
+    title: 'Victory at NITK',
     date: 'January 2025',
     location: 'Surathkal, Karnataka',
     preview: 'How Point Blank claimed their position at the biggest NIT in India',
-    story: [ "Now the NITK Hackverse Hackathon is a big deal for Point Blank. Not only is it the biggest hackathon of the top NIT in India, the lore goes that our seniors had previously gone to this hackathon during their college days but were defeated. Hence, this battle was personal.",
+    story: [ "Now the NITK Hackverse Hackathon is a big deal for Point Blank. Not only is it the biggest hackathon of the top NIT in India, the lore goes that their seniors had previously gone to this hackathon during their college days but were defeated. Hence, this battle was personal.",
       "Point Blank not only went and coded for 24 hours straight in the hackathon, they also got the time to build lore in the infamous NITK Beach, where they frequently ran away to when their code was not working",
-      "That Year, PB had 3 winning teams at NITK, With Point Blank also becoming the grand winners of NITK. We simply followed the instructions of our seniors : 'Win so much that they never invite us again!'.",
-      "We are pretty sure that the NITK Students were left puzzled, that what is this group of arrogant elitists wearing this Point Blank Merch doing here and why are they winning so much? Well needless to say,the post hackathon celebration in Manglore was one to remember."            
+      "That Year, PB had 3 winning teams at NITK, With Point Blank also becoming the grand winners of NITK. They simply followed the instructions of their seniors : 'Win so much that they never invite us again!'.",
+      "Point Blank is pretty sure that the NITK Students were left puzzled, that what is this group of arrogant elitists wearing this Point Blank Merch doing here and why are they winning so much? Well needless to say, the post hackathon celebration in Manglore was one to remember."            
     ],
     images: [
       'https://res.cloudinary.com/pbsite/image/upload/v1748082147/lore/muvpupc59yfidptxuhh5.webp',
@@ -56,8 +56,8 @@ export const Lores: Lore[] = [
     location: 'Shivagange, Karnataka',
     preview: 'Post SIH 2024 and IICT 2024, Point Blank deserved a break again.',
     story: [
-      "Beleive it or not, Organising the internal round of SIH has been something Point Blank has been doing for years and years. Just that this time, the sheer number of teams and the number of projects was something that was never seen before. That combined with organising IICT 2024 meant that Point Blank was exhausted and needed a break again.",
-      "Somehow it is our natural tendency to find a grind even in our relaxation arc. We ended up booking a bus to Shivagange Trek and that too when the rain from previous night had made the trek slippery and dangerous.Well that didnt stop us from starting from banglore at 4am and climbing all the way to the top, fighting monkeys and the urge to simply jump off.",
+      "Believe it or not, organizing the internal round of SIH has been something Point Blank has been doing for years and years. Just that this time, the sheer number of teams and the number of projects was something that was never seen before. That combined with organizing IICT 2024 meant that Point Blank was exhausted and needed a break again.",
+      "Somehow it is their natural tendency to find a grind even in their relaxation arc. They ended up booking a bus to Shivagange Trek and that too when the rain from previous night had made the trek slippery and dangerous. Well that didn't stop them from starting from Bangalore at 4am and climbing all the way to the top, fighting monkeys and the urge to simply jump off.",
       "That year, PB had 3 teams winning the Smart India Hackathon, once again beating the records of all the previous years. Point Blank strongly feels like the monkeys had something to do with it.",
     ],
     images: [
